a8c085f8feace78c14eb64534f2ae8bf3af7b247
Hawleywoo <alhawley21@gmail.com>
2021-08-16 17:17:31 -0600

    Fix merge

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import './Styles/App.scss';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        hello
-      </header>
+      
     </div>
   );
 }
